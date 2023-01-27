6cc9238aa18eea09576ff64a8b9026043e64ec1b
dap2468 <dap2468@gmail.com>
2023-01-27 14:23:16 -0500

    potential divider function was added

--- app.py
+++ app.py
@@ -13,3 +13,16 @@ def parallel(r):
         print ("not enough resistors")
 
 parallel([1000,330,2200])
+
+
+def potential_divider(v,r):
+    sum=0
+
+    for x in r :
+        sum+=x
+    
+    for y in r:
+        v_specific = (y/sum)*v
+        print ("%.2f"%v_specific,"v")
+
+potential_divider(9,[3000,1000])
